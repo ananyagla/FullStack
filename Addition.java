f8d0bf080e7c022774310d7f3e76b921ccad3302
Ananya Chaurasia <ananyachaurasia74@gmail.com>
2025-08-04 15:04:50 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+        int sum = num1 + num2;  // perform addition
+
+        System.out.println("Sum: " + sum);
+    }
+}
+ 
+    
+
